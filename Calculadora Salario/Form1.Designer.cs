fd943769f7374c97f2df5f8261863c7417ccbced
oJoaoVitorr <aserola30@gmail.com>
2026-03-18 13:43:22 -0300

    esqueci o fixedsingle da borda

--- Calculadora Salario/Form1.Designer.cs
+++ Calculadora Salario/Form1.Designer.cs
@@ -249,6 +249,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Calculadora Salário Liquido";
